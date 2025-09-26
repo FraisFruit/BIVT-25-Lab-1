460161cb3cdf0d91367363c98e374a78a42a65c0
Vladimir <vmatusyak07@gmail.com>
2025-09-26 19:32:21 +0300

    Update Blue.cs

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,7 @@
-﻿namespace Lab1
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace Lab1
 {
     public class Blue
     {
@@ -7,9 +10,9 @@
             bool answer = false;
 
             // code here
-
+            if (a != 0 && b != 0 && a * b > 0)
+                answer = true;
             // end
-
             return answer;
         }
         public bool Task2(double d)
@@ -17,9 +20,12 @@
             bool answer = false;
 
             // code here
+            double c = Math.Floor(d);
+            if (d > c || d < c)
+                answer = true;
 
             // end
-
+            Console.WriteLine(answer);
             return answer;
         }
         public bool Task3(int a, int b)
@@ -27,7 +33,8 @@
             bool answer = false;
 
             // code here
-
+            if (b != 0 && a % b == 0)
+                answer = true;
             // end
 
             return answer;
@@ -37,17 +44,34 @@
             double answer = 0;
 
             // code here
+            double d2;
+            double f2;
+            double g2;
+            Console.WriteLine($"{d}, {f}, {g}");
+            d2 = Math.Abs(d);
+            f2 = Math.Abs(f);
+            g2 = Math.Abs(g);
+            if (d2 > f2 && d2 > g2)
+                answer = d;
+            else if (f2 > d2 && f2 > g2)
+                answer = f;
+            else if (g2 > d2 && g2 > f2)
+                answer = g;
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public double Task5(double x)
         {
             double answer = 0;
 
             // code here
-
+            if (x <= -1)
+                answer = 0;
+            else if (x > -1 && x <= 0)
+                answer = x + 1;
+            else
+                answer = 1;
             // end
 
             return answer;
@@ -57,7 +81,10 @@
             bool answer = false;
 
             // code here
-
+            double k = Math.Sqrt(squareS);
+            double c = Math.Sqrt(circleS / Math.PI);
+            if (c <= (k / 2))
+                answer = true;
             // end
 
             return answer;
@@ -68,7 +95,16 @@
             int answer = 0;
 
             // code here
-
+            if (Math.Abs(d) < Math.Abs(f))
+                if (d > 0)
+                    answer = -1;
+                else
+                {
+                    return answer;
+                }
+            else if (f > 0)
+                    answer = 1;
+              
             // end
 
             return answer;
@@ -78,9 +114,19 @@
             bool answer = false;
 
             // code here
-            
+            double a2 = a / 2;
+            double b2 = b / 2;
+            double c2 = c / 2;
+            a2 = Math.Floor(a2);
+            b2 = Math.Floor(b2);
+            c2 = Math.Floor(c2);
+            double sum = a2 + b2 + c2;
+            if (sum % 3 == 0 && sum >= 3)
+                answer = true;
+            if (sum % 3 == 2 && sum >= 2)
+                answer = true;
             // end
-
+            Console.WriteLine(answer);
             return answer;
         }
     }
